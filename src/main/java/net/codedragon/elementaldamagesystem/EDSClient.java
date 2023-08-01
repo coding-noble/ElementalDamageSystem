8582360d09861315c24671d774e367c4d59a012d
coding-noble <milosdragons@gmail.com>
2023-08-01 11:37:23 -0600

    Initial commit

---
+++ src/main/java/net/codedragon/elementaldamagesystem/EDSClient.java
@@ -0,0 +1,10 @@
+package net.codedragon.elementaldamagesystem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EDSClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
